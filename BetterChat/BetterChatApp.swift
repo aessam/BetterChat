002659de0a9b6dd7c8fb2be1fa13cc6aea313051
Ahmed Essam <aessam@me.com>
2025-07-18 11:50:16 -0700

    Initial Commit

---
+++ BetterChat/BetterChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterChatApp.swift
+//  BetterChat
+//
+//  Created by Ahmed Essam on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
